484536edd0edc257ac1c2d8a7035b03f69f6a9ba
Max Dabagia <maxdabagia@gmail.com>
2019-06-24 00:41:11 -0400

    Added cluster matching

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,9 @@ import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 from scipy.linalg import sqrtm
+from scipy.stats import mode
+from sklearn.neighbors import NearestNeighbors
+from itertools import permutations
 from mpl_toolkits.mplot3d import Axes3D
 
 def load_data(file_name):
@@ -28,24 +31,50 @@ def LS_oracle(X_test, Y_test):
     H_inv = X_n.T @ np.linalg.pinv(Y_test).T
     return H_inv
 
-def color_data(X, id):
-    for i in np.unique(id):
-        plt.plot(X[id == i, 0], X[id == i, 1], linestyle='', marker='.', markersize=15)
+def plot_2d_clusters(X, labels):
+    for i in np.unique(labels):
+        plt.plot(X[labels == i, 0], X[labels == i, 1], linestyle='', marker='.', markersize=15)
 
 
-def color_data_3D(X, id):
-    id = np.reshape(np.array(id), len(id))
-    id_size = np.amax(id)
-    data_size = X.shape[1]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    for i in range(id_size + 1):
-        ax.scatter(X[id == i, 0], X[id == i, 1], X[id == i, 2], marker='.')
+def plot_3d_clusters(X, labels):
+    for i in np.unique(labels):
+        plt.scatter(X[id == i, 0], X[id == i, 1], X[id == i, 2], marker='.')
 
 
 def eval_R2(X, Y):
     X = normal(X)
     Y = normal(Y)
     return 1 - np.mean(np.power(Y - X, 2), axis=0).sum() / np.var(Y, axis=0).sum()
+
+def match_clustered_labels(X, X_labels, Y, Y_labels):
+    """Find the optimal permutation of labels found by clustering, via 1-nearest neighbors classification accuracy.
+
+    Parameters
+    ----------
+    X : array-like, shape (n_samples_x, n_features)
+        Dataset of labels to match to.
+
+    X_labels : array-like, shape (n_samples_x, )
+        Labels to match to.
+
+    Y : array-like, shape (n_samples_y, n_features)
+        Dataset of labels to be matched.
+
+    Y_labels : array-like, shape (n_samples_y, )
+        Labels to be matched.
+
+    Returns
+    ----------
+    Y_new : New Y labels.
+
+    """
+    nbrs = NearestNeighbors()
+    nbrs.fit(X)
+    idx = nbrs.kneighbors(Y, n_neighbors=1, return_distance=False).squeeze()
+
+    Y_new = np.zeros(Y_labels.shape, dtype=int)
+    for i in np.unique(Y_labels):
+        idc = idx[Y_labels == i]
+        Y_new[Y_labels == i], _ = mode(X_labels[idc])
+
+    return Y_new
